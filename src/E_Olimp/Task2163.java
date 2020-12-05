476b22a8824f328339d3c8138a48d10cf8a6c1fb
Gunay-Jafar <cafarligunay06@gmail.com>
2020-12-05 20:17:57 +0400

    Task2163 from E-Olimp

--- src/E_Olimp/Task2163.java
+++ src/E_Olimp/Task2163.java
@@ -1,2 +1,26 @@
-package E_Olimp;public class Task2163 {
+package E_Olimp;
+
+import java.util.Scanner;
+
+//100
+public class Task2163 {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String str=scanner.nextLine();
+
+        int cem=0;
+
+        for (int i=0;i<str.length();i++){
+           char ch= str.charAt(i);
+           int a=Character.getNumericValue(ch);
+           cem+=a;
+        }
+
+        if (cem%3==0){
+            System.out.println("YES");
+        }else
+            System.out.println("NO");
+
+    }
 }
